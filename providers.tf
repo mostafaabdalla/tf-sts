0fe1cb976712733ecbc84c89725996892fac4f66
mostafa <mostafaa.abdallaa@gmail.com>
2022-02-24 21:19:34 +0000

    modified the providers file

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
   assume_role {
-    role_arn     = "arn:aws:iam::663119550079:role/sts-role"
-    session_name = "demo-sts"
+    role_arn     = "<role_arn>"
+    session_name = "demo"
   }
 }
